49106d1171633ff4f40a144c50a1345c717a16f2
@everyone <69013364+UnbanMeFromPoggit@users.noreply.github.com>
2020-07-31 01:32:37 +0400

    Create Main.php

---
+++ src/something/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace something;
+
+use pocketmine\PluginBase;
+
+class Main extends PluginBase{
+
+    public function onEnable(): void{
+        $this->getLogger()->notice("Hello");
+    }
+}
